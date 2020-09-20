625c7872da19e4a168bc0527c98c0077c90d692e
soup-bowl <code@soupbowl.io>
2020-09-20 16:06:42 +0100

    Pin config noted.

--- btn.py
+++ btn.py
@@ -1,15 +1,20 @@
 from time import sleep
 from gpiozero import Button, LED
 
-button = Button("BOARD22", False)
+button = Button("BOARD22", False, None)
 light  = LED("BOARD7")
 
+def led_blink(count):
+    for x in range(count):
+        light.on()
+        sleep(0.25)
+        light.off()
+        sleep(0.25)
+
+# Operation area.
+
 print("Light test.")
-for x in range(5):
-    light.on()
-    sleep(0.25)
-    light.off()
-    sleep(0.25)
+led_blink(5)
 
 print("Test done, waiting for command...")
 while True:
--- readme.md
+++ readme.md
@@ -5,3 +5,10 @@ This is very much experimental, and is based upon custom wiring. Your mileage mi
 ```
 sudo apt install python3-rpi.gpio python3-gpiozero
 ```
+
+# Pin Connections
+In this setup, the following is assumed (board, not Broadcom SOC):
+```
+07 | Tube button. 
+22 | Tube LED (Mosfet).
+```
